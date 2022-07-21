4d35abe246fd6ed473ee40e8b583c3a608b349d5
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-07-21 09:33:48 +0100

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        print("{}{}".format(x, y), end=", ")
+        if (not (x == 8 and y == 9)):
+            print("{}{}".format(x, y), end=", ")
+        else:
+            print("{}{}".format(x, y))
