30ae270e6f4139fe7b3169519672ae16d5322612
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-08-02 15:39:22 +0100

    tasks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
     """creating an empty class called square"""
 class Square:
-    """representing an empty class square."""
+    """representing an empty class square.
+In empty class nothing is initiaze and pass is used 
+    """
     pass
