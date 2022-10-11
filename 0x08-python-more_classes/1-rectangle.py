29463d9a1597dfea16857913f19aae86648148b9
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-10-11 11:20:44 +0100

    tasks

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ creating a rectangle with getter and setter method.
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-           raise TypeError('width must be an integer')
+            raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
@@ -32,7 +32,7 @@ creating a rectangle with getter and setter method.
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-           raise TypeError('height must be an integer')
+            raise TypeError('height must be an integer')
         elif value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
