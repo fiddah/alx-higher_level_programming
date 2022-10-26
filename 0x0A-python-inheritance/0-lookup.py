e32e0c2c7d527cb8130a1dfae2a3e6a278e8653d
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-10-26 17:28:15 +0100

    tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" creating a function that returns a list of available
+attributes and methods of an object
+"""
+
+def lookup(obj):
+    """ a function tha returns a list of object"""
+    return dir(obj)
