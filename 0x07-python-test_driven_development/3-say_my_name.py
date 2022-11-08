1733b41c037c024112b047b17fd39da31484cdf8
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-08 16:44:19 +0100

    tasks

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     """ first name and last name must be a string """
-    if first_name != str:
+    if type(first_name != str) or first_ name != "":
         raise TypeError('first_name must be a string')
-    if last_name != str:
+    if type(last_name) != str:
         raise TypeError('last_name must be a string')
     print('My name is {} {}' .format(first_name, last_name))
