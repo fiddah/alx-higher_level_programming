9de67a063a929578566d4d4fa2da9e22c538323c
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-18 19:32:31 +0100

    tasks

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""instance of a class and inherited class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """isinstance of class and inherited class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
