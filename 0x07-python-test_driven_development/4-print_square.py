427d40d9acd3892e0006339ac41f3055277689af
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-18 15:03:45 +0100

    tasks

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""""A module to print a square with the character # """
+
+
+def print_square(size):
+    """a print square function"""
+    if size not isinstance(size, int):
+        raise TypeError('size must be integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if size isinstance(size, float) and size < 0:
+        raise TypeError('size must be integer')
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
