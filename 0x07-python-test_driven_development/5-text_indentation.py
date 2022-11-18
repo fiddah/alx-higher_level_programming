29eb79c62128ee16d16fdbe1d899a80130d9a616
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-18 15:49:58 +0100

    tasks

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" A  function that prints a text with 2 new lines after each of these characters: ., ? and :."""
+""" A  function that prints a text with 2 new 
+lines after each of these characters: ., ? and :."""
 
 
 def text_indentation(text):
-    """A  function that prints a text with 2 new lines after each of these characters: ., ? an    d :."""
+    """A  function that prints a text with 2 new lines after
+    each of these characters: ., ? an    d :."""
     if type(text) != str:
         raise TypeError('text must be a string')
     flag = 0
