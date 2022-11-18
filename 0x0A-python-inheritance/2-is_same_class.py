69c706da9d331237584600a923de0aa74973d4cc
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-18 19:12:41 +0100

    tasks

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,7 +7,7 @@
 
 def is_same_class(obj, a_class):
     """is an instance of class"""
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     else:
         return False
