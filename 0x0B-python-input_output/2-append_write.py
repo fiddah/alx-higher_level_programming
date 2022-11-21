d753b5f2503617d6455eed9e55576766b5dfd859
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-21 11:04:16 +0100

    tasks

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_file(filename="", text=""):
     """appends a string at the end of a texts file"""
-    with open(filename, mode="a", encoding="utf-8") as f:
+    with open(filename, mode="a") as f:
         return f.write(text)
