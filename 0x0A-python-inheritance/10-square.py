1c60f4762116e162b6ae2243fa4b5c2910cef4e9
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-21 09:24:54 +0100

    tasks

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Retangle):
+    """Square class inherits from Rectangle"""
+    def __init__(self, size):
+        """initializing"""
+        self.__size = size
+        """validating size"""
+        self.integer_validator("size", size)
+
+    def area(self):
+        """A method that returns the area of a square."""
+        return self.__size * self.__size
