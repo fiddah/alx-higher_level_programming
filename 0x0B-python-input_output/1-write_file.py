1168a4f36c5152d5818d8cc383b9dd291938afd1
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-21 10:44:53 +0100

    tasks

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Ä module that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """write a string to a texts file"""
+    with open(filename, mode="w" encoding="utf-8") as f:
+        print(f.write(text),)
