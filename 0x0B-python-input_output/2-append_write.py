818729b9e02cecec7a45a35021d88c9d3d803499
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-21 11:00:08 +0100

    tasks

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Ä module that appends a string at the end of a text file"""
+
+
+def append_file(filename="", text=""):
+    """appends a string at the end of a texts file"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.append(text)
