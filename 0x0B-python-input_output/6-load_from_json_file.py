08ef8c38274f63b4b37b4bf5beb0c839bb56aa76
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-21 11:38:34 +0100

    tasks

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Json representation from to object"""
+
+import json
+
+
+def load_from_json_file(my_obj, filename):
+    """creates an Object from a “JSON file”"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(json.loads(filename))
