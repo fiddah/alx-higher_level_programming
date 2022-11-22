0aa4fa79d66ed50885f8c51dd26d21d87c19e96c
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-22 15:08:30 +0100

    tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """return the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
