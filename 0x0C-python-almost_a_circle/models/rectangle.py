475a8dbe76cd4293eaa8cf98ea9127b0ad477e72
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-22 14:44:37 +0100

    tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -115,3 +115,11 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Rectangle"""
+        return {'id': self.id
+                'width': self.width
+                'height': self.height
+                'x': self.x
+                'y': self.y}
