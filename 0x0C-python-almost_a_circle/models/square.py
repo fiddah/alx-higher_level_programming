e4fbc886c67976295e33cfecffb3a1fab5764155
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-22 14:12:19 +0100

    tasks

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Square module"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Creating a rectangle class that inherits from the base class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializing"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """informal string representation of the square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)    
