5f605bf7618709e62c8e3b60572dbfc71576ed4d
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-22 11:05:39 +0100

    tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,11 @@
 class Base():
     """creating a class Base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id != None:
+        """initializing base class"""
+        if id is None:
             Base.__nb_objects = +1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
         else:
             self.id = id
