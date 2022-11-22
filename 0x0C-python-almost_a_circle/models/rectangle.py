8e63b05a0b043d64ca5532f068557c13e065eafd
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-22 11:37:18 +0100

    tasks

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """Creating a rectangle class that inherits from the base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initializing"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__.(id)
+
+    @property
+    def width(self):
+        """return a private attribute"""
+        return self.__width
+
+    @property
+    def height(self):
+        """return a private attribute"""
+        return self.__height
+
+    @property
+    def x(self):
+        """return a private attribute"""
+        return self.__x
+
+    @property
+    def y(self):
+        """return a private attribute"""
+        return self.__y
