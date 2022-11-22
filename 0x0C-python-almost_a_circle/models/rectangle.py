51eb1384b64ca4eab8799cb6c3495c745d97daf6
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-11-22 13:46:03 +0100

    tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,4 +84,7 @@ class Rectangle(Base):
     def __str__(self):
         """A method that creates a string object from a given object"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-            self.__x, self.__y, self.__width, self.__height)
+                                                       self.__x, 
+                                                       self.__y, 
+                                                       self.__width, 
+                                                       self.__height)
