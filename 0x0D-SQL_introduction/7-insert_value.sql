7e4c286dd8ceb03d265219deb7d40fb90a6cfcfd
fiddah <49383326+fiddah@users.noreply.github.com>
2022-12-05 18:40:56 +0100

    tasks

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- inserting new rows
-INSERT INTO `first_table`(`1d`, `name`) VALUES (89, 'Best School');
-SELECT COUNT(*) FROM first_table WHERE id=89;
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
