daae58dcbfab020c520d0f07d329c55332794e82
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-12-08 12:53:28 +0100

    tasks

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXIST hbtn_0d_usa
+CREATE TABLE IF NOT EXIST hbtn_0d_usa.states(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL)
