24d98a365ed91d1102d611e06965956caebbf60e
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-12-08 12:28:58 +0100

    tasks

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- create a table even if it exist
-CREATE TABLE IF NOT EXISTS force_table(id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
