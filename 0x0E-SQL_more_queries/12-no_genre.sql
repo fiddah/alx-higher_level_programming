bb52868b0c13256dc19c819883dc297c72cd319d
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-12-08 17:13:41 +0100

    tasks

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,10 @@
--- lists all shows contained in the database hbtn_0d_tvshows.
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- Each record displays: tv_shows.title - tv_show_genres.genre_id
 -- Result is sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- If a show doesn’t have a genre, it displays NULL
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
