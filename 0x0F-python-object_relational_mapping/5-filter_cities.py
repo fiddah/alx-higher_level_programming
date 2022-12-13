795a93e495b34c4bdf47c2ec6296d23dee2bc8e3
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-12-13 17:41:23 +0100

    tasks

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,4 +13,7 @@ if __name__ == "__main__":
     c.execute("SELECT cities.name FROM cities
                JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s
                ORDER BY cities.id", (argv[4],))
-    [print(", ".join(city[0]for city in c.fetchall())]
+    rows = c.fetchall()
+    print(", ".join(city[0] for city in rows))
+    c.close()
+    db.close()
