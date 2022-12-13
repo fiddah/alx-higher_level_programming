7686675e8af796dfd67e2ca8bdb8b079cec4cf75
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-12-13 12:18:16 +0100

    tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,12 +2,12 @@
 """
 a script that takes in arguments and displays all values in the states
 """
-from sys import argv
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` WHERE name LIKE %s ORDER BY id ASC",
-              (argv[4],))
+              (sys.argv[4],))
     [print(row) for row in c.fetchall()]
