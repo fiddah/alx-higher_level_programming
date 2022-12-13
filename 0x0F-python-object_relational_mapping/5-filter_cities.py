bc50699f2060638638ef6456c084ae201415b7a7
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-12-13 17:36:27 +0100

    tasks

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+python script that lists all cities from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT cities.name FROM cities
+               JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s
+               ORDER BY cities.id", (argv[4],))
+    [print(", ".join(city[0]for city in c.fetchall())]
